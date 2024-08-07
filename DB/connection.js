d1b6c1b3bf80650d97c18a123281914416483440
AlaaAlShalaldeh <alaaalshalaldeh3@gmail.com>
2024-08-07 21:00:25 +0300

    database connection

--- DB/connection.js
+++ DB/connection.js
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize';
-export const sequelize = new Sequelize('plogproject', 'root', '', {
-    host: 'localhost',
-    dialect:"mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-  });
-// export const sequelize = new Sequelize('freedb_plog_project', 'freedb_alaa_Sh', 'c45XC&57#pMxdQu', {
-//     host: 'sql.freedb.tech',port: 3306,
+// export const sequelize = new Sequelize('plogproject', 'root', '', {
+//     host: 'localhost',
 //     dialect:"mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 //   });
+export const sequelize = new Sequelize('freedb_plog_project', 'freedb_alaa_Sh', 'c45XC&57#pMxdQu', {
+    host: 'sql.freedb.tech',port: 3306,
+    dialect:"mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  });
  export const connectedDB =async()=>{
     try {
        return await sequelize.sync({alter:false});
